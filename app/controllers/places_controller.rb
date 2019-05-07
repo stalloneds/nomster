334a95bb11b5fd7032b66ecd38ad3bf0b6e0e6aa
Stallone Dias <stalloneds@gmail.com>
2019-05-07 15:15:15 +0000

    added edit form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,6 +14,14 @@ class PlacesController < ApplicationController
 		redirect_to root_path 
 	end 
 
+	def show 
+		@place = Place.find(params[:id])
+	end 
+
+	def edit 
+		@place = Place.find(params[:id])
+	end 
+
 	private 
 
 	def place_params
