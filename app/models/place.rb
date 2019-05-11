44afbff610affc2328f72d994c6d3072d3c52946
Stallone Dias <stalloneds@gmail.com>
2019-05-11 15:36:52 +0000

    added comments database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
 	belongs_to :user
+	has_many :comments
 	validates :address, :description, presence: true
 	validates :name, presence: true, length: { minimum: 3 }
 	geocoded_by :address
